ea719c8bb25fe4a80f05900f5d137ca5005a3ddc
yashraj <yam.02raj@gmail.com>
2026-09-16 16:12:14 +0530

    Python program to add two numbers

---
+++ chapter2/practiceSet/01_problem.py
@@ -0,0 +1,6 @@
+# Python program to add two numbers
+a = int(input("enter a number: "))
+b = int(input("enter a number: "))
+print("number is: ", a)
+print("number is: ", b )
+print("the sum is: ", a + b)
