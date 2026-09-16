9c18a6f7ab584342729978ecaf14eac58967fb80
yashraj <yam.02raj@gmail.com>
2026-09-16 16:14:54 +0530

    Finding the Square of number

---
+++ chapter2/practiceSet/06_problem.py
@@ -0,0 +1,4 @@
+#  Python program to calculate the square of a number entered by the user.
+a = int(input("Enter a number whose square you want to know: "))
+sqr = a**2
+print("Sqaure of ", a , "is: ", sqr)
