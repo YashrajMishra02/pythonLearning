e8b797a39129693d5bbe44f52890c976d534fc73
yashraj <yam.02raj@gmail.com>
2026-09-16 16:10:58 +0530

    Learn multiple value assignment and deletion of variable

--- chapter2/01_variables.py
+++ chapter2/01_variables.py
@@ -1,3 +1,16 @@
 a = 1
 b = 2
 print(a+b)
+
+# Assigning same values to variable 
+a = b = c = 10
+print (a , b ,c)
+
+# Assigning different values to variable 
+x, y, z = 10,"hi", 2.52
+print(x , y, z)
+
+# deleting variable
+x = 10
+del(x)
+print(x)
