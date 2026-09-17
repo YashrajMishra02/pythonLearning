bbd0d0fae9a2711e35784eb66800a16184f43e8c
yashraj <yam.02raj@gmail.com>
2026-09-17 12:18:03 +0530

    Some Important functions of string

---
+++ chapter3/02_str_function.py
@@ -0,0 +1,11 @@
+name = "the Hanuman is son of lord shiv and kesari"
+print(name.capitalize())
+print(name.count("s"))
+print(name.find("is"))
+print(name.startswith("the"))
+print(name.endswith("i"))
+print(name.replace("shiv","anjani"))
+print(name.find("l"))
+print(name.split(' '))
+# traversing from end to beginning
+print(name[::-1])
