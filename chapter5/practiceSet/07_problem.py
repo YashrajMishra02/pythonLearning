7ff367caf0028b66fcec3d0733a9c57688da19c4
yashraj <yam.02raj@gmail.com>
2026-09-21 19:28:38 +0530

    Dictonary contain unique key and value pair if conatin same key and different value the value is updated

---
+++ chapter5/practiceSet/07_problem.py
@@ -0,0 +1,15 @@
+# If the names of 2 friends are same; what will happen to the program in problem 6?
+
+d = {}
+name = input("Enter Friends name: ")
+lang = input("Enter Favourite lang: ")
+d.update({name:lang})
+
+name = input("Enter Friends name: ")
+lang = input("Enter Favourite lang: ")
+d.update({name:lang})
+
+name = input("Enter Friends name: ")
+lang = input("Enter Favourite lang: ")
+d.update({name:lang})
+print(d)
