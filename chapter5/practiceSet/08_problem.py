b0138bd058e5e5c018b0332dab59510d54482fa5
yashraj <yam.02raj@gmail.com>
2026-09-21 19:30:32 +0530

    Dictonary may contain same value

---
+++ chapter5/practiceSet/08_problem.py
@@ -0,0 +1,15 @@
+# If languages of two friends are same; what will happen to the program in problem 6?
+
+d = {}
+name = input("Enter Friends name: ")
+lang = input("Enter Favourite lang: ")
+d.update({name:lang})
+
+name = input("Enter Friends name: ")
+lang = input("Enter Favourite lang: ")
+d.update({name:lang})
+
+name = input("Enter Friends name: ")
+lang = input("Enter Favourite lang: ")
+d.update({name:lang})
+print(d)
