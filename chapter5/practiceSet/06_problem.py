077f6093853935a4dcab35399d9d22574030a7e3
yashraj <yam.02raj@gmail.com>
2026-09-21 19:26:38 +0530

    user input its key and value into dictonary using 'update' function of dictonary

---
+++ chapter5/practiceSet/06_problem.py
@@ -0,0 +1,6 @@
+# Create an empty dictionary. Allow 4 friends to enter their favorite language as value and use key as their names. Assume that the names are unique.
+
+d = {}
+name = input("Enter Friends name: ")
+lang = input("Enter Favourite lang: ")
+d.update({name:lang})
