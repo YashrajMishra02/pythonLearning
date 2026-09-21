49f94fec97cd63ffc9678c4a0bdbf49e9cbc63d1
yashraj <yam.02raj@gmail.com>
2026-09-21 19:20:19 +0530

    Program to create a dictionary of Hindi words with values as their English translation. Provide user with an option to look it up!

---
+++ chapter5/practiceSet/01_problem.py
@@ -0,0 +1,9 @@
+# Program to create a dictionary of Hindi words with values as their English translation. Provide user with an option to look it up!
+
+d = {
+    "ghadi": "clock",
+    "kanghi":"comb",
+    "kitab":"books"
+}
+word = input("Enter the word of which you want English meaning: ")
+print(d[word])
