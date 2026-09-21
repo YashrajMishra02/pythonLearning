b2881615b18c9e746a3dd17e9ff33118ca62fb71
yashraj <yam.02raj@gmail.com>
2026-09-21 19:18:40 +0530

    Learn Dictonary, what's key : value pair and its built in functions

---
+++ chapter5/01_dict.py
@@ -0,0 +1,15 @@
+# create a empty dectionary
+# d = {}
+# print(type(d))
+marks = {
+    "yashraj":100,
+    "anup":99,
+    "deep":98
+}
+# print(marks,type(marks))
+# print(marks["anup"])
+# print(marks.items())
+# print(marks.keys())
+# marks.update({"friend":99.7})
+# print(marks)
+print(marks.get("deep"))
