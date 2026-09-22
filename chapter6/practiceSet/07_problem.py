b500848c0ee174e655af01c288e4a104898cc608
yashraj <yam.02raj@gmail.com>
2026-09-22 22:25:06 +0530

    Using Membership operator (i.e. in and not in) as a condition in the if else condition

---
+++ chapter6/practiceSet/07_problem.py
@@ -0,0 +1,6 @@
+# program to find out whether a given post is talking about "jack" or not
+s = input("Enter the post: ")
+if(("jack" in s) or ("jack" in s)):
+    print("given post talk about yashraj")
+else:
+    print("does not talk about jack")
