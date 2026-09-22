93a73df46d0f92de4af3269e63412fd86f19986a
yashraj <yam.02raj@gmail.com>
2026-09-22 22:20:33 +0530

    Using builtin function 'collection'  used counter to check if one string is the permutation of other string

---
+++ chapter3/practiceSet/06_problem.py
@@ -0,0 +1,9 @@
+# check palindrome
+from collections import Counter
+def ispermutation(str1, str2):
+    return Counter(str1) == Counter(str2)
+
+str1 = input("Enter a string: ")
+str2 = input("Enter a string: ")
+
+print(f"{ispermutation(str1, str2)} it is permutation of each other")
