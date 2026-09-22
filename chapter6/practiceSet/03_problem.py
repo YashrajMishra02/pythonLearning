9ee86c7860fd2abc1f3434b4a54c52006995e235
yashraj <yam.02raj@gmail.com>
2026-09-22 22:17:47 +0530

    Spam comment is defined as a text containing keyword

---
+++ chapter6/practiceSet/03_problem.py
@@ -0,0 +1,11 @@
+# Spam comment is defined as a text containing keywords:
+p1 = "Make a lot of money"
+p2 = "buy now"
+p3 = "subscirbe this"
+p4 = "click this"
+
+message = input("Enter your comment: ")
+if((p1 in message) or(p2 in message) or (p3 in message) or(p4 in message)):
+    print("This comment is a spam")
+else:
+    print("Comment is not a spam")
