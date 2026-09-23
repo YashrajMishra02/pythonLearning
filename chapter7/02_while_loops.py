6470c0f425bb19c5c3268a27e9809e58e824c68e
yashraj <yam.02raj@gmail.com>
2026-09-23 22:01:53 +0530

    Learn while loop and used in list

---
+++ chapter7/02_while_loops.py
@@ -0,0 +1,18 @@
+# i = 1
+# while(i<=6):
+#     print(i)
+#     i+=1
+
+# write a program to print 1 to 50 using a while loop
+# i = 1
+# while(i<=50):
+#     print(i)
+#     i+=1
+
+# write a program to print the content of a list using while loop
+l = [1,2,3,445,"hi","namaste", 334.54,"jai","shree","ram","jai","bajaran","bali",8693873399,"etc"]
+i = 0
+while(i<len(l)):
+    print(l[i])
+    i +=1
+print(len(l))
