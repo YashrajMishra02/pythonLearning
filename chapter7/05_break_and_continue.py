2663944edbfff3c27adab0c2eaa984ac3a2d83b7
yashraj <yam.02raj@gmail.com>
2026-09-23 22:03:11 +0530

    Break and Continue statement in loop

---
+++ chapter7/05_break_and_continue.py
@@ -0,0 +1,13 @@
+# Break statement
+# for i in range(0,80):
+#     if(i==50):
+#         break
+#     print(i)
+
+# Continue statement
+for i in range(0,80):
+    if(i==50):
+        continue
+    print(i)
+
+
