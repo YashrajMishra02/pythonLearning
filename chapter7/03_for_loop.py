a10d687183c22fb678975c76d4c2f300823c2416
yashraj <yam.02raj@gmail.com>
2026-09-23 22:02:20 +0530

    Using for loop in different data types like list, tuple and string

---
+++ chapter7/03_for_loop.py
@@ -0,0 +1,17 @@
+# for i in range(1,6):
+#     print(i)
+
+# for loop with Lists
+# l =[1,4,72,2,"hello"]
+# for item in l:
+#     print(item)
+
+# for loop with Tuples
+# l = (23,1,36,'h2',34.23)
+# for item in l:
+#     print(item)
+
+# for loop with string
+s = "yashraj"
+for item in s:
+    print(item)
