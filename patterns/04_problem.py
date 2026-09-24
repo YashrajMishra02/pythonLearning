36579797ff6018be1414c2cfeae2001fdb145d8f
yashraj <yam.02raj@gmail.com>
2026-09-24 22:00:00 +0530

    Progarm to print rectangle start pattern

---
+++ patterns/04_problem.py
@@ -0,0 +1,10 @@
+# progarm to print rectangle start pattern
+# ***
+# ***
+# ***
+
+n = int(input("Enter number of rows want in star pattern: "))
+i = 1
+while(i <= n):
+    print("*" * n)
+    i+=1
