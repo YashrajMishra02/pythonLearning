c82f1360d3037c5c631aa353b3ed99a852cb9273
yashraj <yam.02raj@gmail.com>
2026-09-26 23:10:49 +0530

    organization oflist methods

--- chapter4/04_tuple_method.py
+++ chapter4/04_tuple_method.py
@@ -1,8 +1,10 @@
 a = (2,5,6,"hanuman",False,1,23,1,4,1)
+
 print(a.count(1))
 print(a.index(1))
 repeated = a *3
 print(repeated)
-# membership       
+
+# membership operator     
 print(2 in a)
 print(34 in a)
