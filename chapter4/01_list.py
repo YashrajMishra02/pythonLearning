4e907ccbda279d28152e5ee009492004e6290c39
yashraj <yam.02raj@gmail.com>
2026-09-26 23:08:47 +0530

    List initilization addition

--- chapter4/01_list.py
+++ chapter4/01_list.py
@@ -1,3 +1,8 @@
+# Program to learn list
+
+courses = [] # Initialization of list
+courese = list() # Initialization of list
+
 friends = ["Apple","Orange",5,346.25,False,"Akash","Rohan"]
 print(friends[0])
 friends[0] = "grapes"
